c35e56452d619425a488149344ff26cd52a6a02a
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-07-23 16:10:11 -0300

    Agregando resolucion

--- ejercicio2-multiples_clientes/2-basics-server.py
+++ ejercicio2-multiples_clientes/2-basics-server.py
@@ -17,16 +17,33 @@ socket_server.bind((host, port))
 print('Servidor activo....')
 socket_server.listen()
 
-# (*) Aca ya no solo tiene que ser aceptar una sola conexion sino infinitas
-## Mientras el servidor este corriendo:
-### Aceptar una nueva conexion (cliente y direccion)
-cliente, address = socket_server.accept()
-### Crear un hilo nuevo que se encargue de ese cliente
-### El hilo tendra la tarea de ejercutar una funcion que maneje solo a ese cliente.
+lista_de_clientes = []
 
 #### Funcion manejar cliente:
+def handle_clients(cliente_socket, addr):
+    connected = True
 # Mientras el cliente este conectado
-## Escuchar los mensajes que envia
-## Si el mensaje es 'salir', cortar conexion y salir del hilo
+    while connected:
+        ## Escuchar los mensajes que envia
+        mensaje = cliente_socket.recv(2048).decode('utf-8')
+        if mensaje != '':
+            print(f'{cliente_socket}:{addr} ha enviado el siguiente mensaje: {mensaje}')
 
-#### Llevar registro de todos los clientes conectados 
+        ## Si el mensaje es 'salir', cortar conexion y salir del hilo
+        if mensaje == 'salir':
+            print(f'Cerrando conexion de {cliente_socket}:{addr}')
+            cliente_socket.close()
+            connected = False       # para romper el loop
+        
+# (*) Aca ya no solo tiene que ser aceptar una sola conexion sino infinitas
+## Mientras el servidor este corriendo:
+while True:
+    ### Aceptar una nueva conexion (cliente y direccion)
+    cliente, address = socket_server.accept()
+    ### Crear un hilo nuevo que se encargue de ese cliente
+    thread_clients = threading.Thread(target=handle_clients, args=(cliente, address))
+    thread_clients.start()
+    ### El hilo tendra la tarea de ejercutar una funcion que maneje solo a ese cliente.
+
+    #### Llevar registro de todos los clientes conectados 
+    lista_de_clientes.append((cliente,address))
