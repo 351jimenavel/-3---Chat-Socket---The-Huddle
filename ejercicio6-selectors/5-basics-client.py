079a7578d8917052617d9881ef57f14828f86fa2
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-07-25 02:56:47 -0300

    Agrego consigna

---
+++ ejercicio6-selectors/5-basics-client.py
@@ -0,0 +1,9 @@
+'''Crear un servidor eco no bloqueante que:
+
+Acepte múltiples clientes.
+
+Use selectors para manejar conexiones (en vez de threads o select.select).
+
+Devuelva a cada cliente lo que le envía (modo eco).
+
+Permanezca activo sin trabarse mientras espera mensajes.'''
