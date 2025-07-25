0aaf7571cc37fae6fa4a1c1b84ac3e2ac37a2e15
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-07-25 02:59:42 -0300

    Agregro solucion del ejercicio 4 para testeo

--- ejercicio6-selectors/5-basics-client.py
+++ ejercicio6-selectors/5-basics-client.py
@@ -7,3 +7,64 @@ Use selectors para manejar conexiones (en vez de threads o select.select).
 Devuelva a cada cliente lo que le envía (modo eco).
 
 Permanezca activo sin trabarse mientras espera mensajes.'''
+
+######### NOTE: Usaremos la resolucion del ejercicio 4 para el lado del cliente ya que el ejercicio propone cambios en el servidor
+
+import socket
+import threading
+## DESDE EL CLIENTE
+# 5. El cliente crea su propio socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 6. El cliente conecta su socket a la misma direccion IP y puerto colocado en el servidor (se establece conexion)
+host = '127.0.0.1'
+port = 5555     # Se agrega un puerto diferente ya que en el socket del servidor se utiliza este
+client_socket.connect((host, port))
+print('CONECTADO AL SERVIDOR')
+
+# El cliente tendra hilos para
+## 1) Enviar mensajes al servidor (lo que el usuario va a escribri)
+def enviar_mensaje(cliente_socket):
+    ## Opcion para salir del chat con comando o escribiendo "salir"
+    while True:
+        try:
+            mensaje = input()
+        except EOFError:
+            print("Se cerro la entrada.")
+            break
+    ## Mientras no se haya escrito esto: pedir al usuario que escriba algo y enviar eso al servidor
+        if mensaje != '':
+            print('Mensaje enviado')
+            client_socket.send(mensaje.encode())
+        else:
+            print('El mensaje enviado por el cliente esta vacio')
+    ## Si el mensaje es salir
+        ## cerrar socket
+        ## terminar hilo
+        if mensaje.lower() == 'salir':
+            print('Cerrando conexion...')
+            cliente_socket.close()
+            break
+
+## 2) Recibir mensajes desde el servidor
+def recibir_mensaje(cliente_socket):
+    ## estar siempre escuchando
+    while True:
+        try:
+            response = client_socket.recv(2048).decode('utf-8')
+            if response:
+                ## mostrar los mensajes recibidos del servidor
+                print(f'Mensaje recibido: {response}')
+            else:
+                print('Cerrando conexion...')
+                cliente_socket.close()
+                break
+        except (ConnectionError, OSError):
+            print('Se perdio la conexion con el servidor')
+            cliente_socket.close()
+            break
+
+thread_envio = threading.Thread(target=enviar_mensaje,args=(client_socket,))
+thread_recepcion = threading.Thread(target=recibir_mensaje, args=(client_socket,))
+thread_envio.start()
+thread_recepcion.start() 
